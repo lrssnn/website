d7b5c9a7123623e2120b113982318af5c136c137
Niklas Larsson <niklaslarsson95@gmail.com>
2018-07-22 16:15:56 +1000

    Textures

--- sketch.js
+++ sketch.js
@@ -3,13 +3,17 @@ var H = 8000;
 var G = 800;
 var R = W / 2;
 
+var lines = [];
+var mem_index = 0;
+var MAX_MEM = 10000;
+
 function setup(){
   createCanvas(windowWidth, windowHeight);
-  colorMode(HSB);
-  background(0, 0, 0);
+  background(0);
   noFill();
   strokeWeight(1);
-  stroke(0, 0, 255);
+  stroke(255);
+  frameRate(-1);
 }
 
 var colour = 0;
@@ -18,16 +22,44 @@ var speed = 0.75;
 
 function draw() {
 
-  translate(width/2 , height/ 2);  scale(0.9);
-  
+  background(0);
+  fill(255);
+  strokeWeight(0.1);
+  text(mem_index, 0, 10);
+  noFill();
+  translate(width/2 , height/ 2);
+
+  scale(500/t);
+
+
   var last_t = t - speed;
-  line(last_t*sin(last_t), last_t*cos(last_t), t * sin(t), t * cos(t))
+  var next_line = {
+    x1 : last_t*sin(last_t),
+    y1 : last_t*cos(last_t),
+    x2 : t * sin(t),
+    y2 : t * cos(t),
+  };
+
+  lines[mem_index] = next_line;
+  mem_index = (mem_index + 1) % MAX_MEM;
+
+  for(var i = 0; i < MAX_MEM; i++) {
+    var m = (mem_index + i) % MAX_MEM;
+    strokeWeight(i/MAX_MEM);
+    var l = lines[m];
+    if(l != undefined){
+      line(l.x1, l.y1, l.x2, l.y2);
+    }
+  }
+
+  // lines.forEach((l,index) => {
+  //   if(l != null){
+  //     strokeWeight(index/MAX_MEM)
+  //     line(l.x1, l.y1, l.x2, l.y2);
+  //   }
+  // })
+  
   t += speed;
   speed = max(0, speed + (random() - 0.5)/100);
 
-  if(t > 500){
-    colour = (colour + 50) % 255;
-    stroke(colour, 255, 255);
-    t = 0;
-  }
 }
